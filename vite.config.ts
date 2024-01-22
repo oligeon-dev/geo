a339b9a5d2f7172196ddc5a761e6ef8af9385264
oligeon-dev <oligeon.dev@gmail.com>
2024-01-22 23:57:03 +0900

    to pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "geo",
+        short_name: "geo",
+        description: "geo sample",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
