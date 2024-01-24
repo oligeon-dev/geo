1b6f7be2a479f065e97d9f4d7f300d81615c1cbf
oligeon-dev <oligeon.dev@gmail.com>
2024-01-24 13:56:24 +0900

    display permission state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [latitude, setLatitude] = useState<number>();
   const [longitude, setLongitude] = useState<number>();
+  const [permission, setPermission] = useState("");
   const onClick = () => {
     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
   };
@@ -12,10 +13,23 @@ function App() {
     setLatitude(position.coords.latitude);
     setLongitude(position.coords.longitude);
   };
+
+  useEffect(() => {
+    const getPermissionState = async () => {
+      const permissionState = await navigator.permissions.query({
+        name: "geolocation",
+      });
+      setPermission(permissionState.state);
+      return permissionState;
+    };
+
+    getPermissionState();
+  }, []);
   const errorCallback = () => {};
   return (
     <>
       <div>version: 0.0.2</div>
+      <p>permission state: {permission}</p>
       <button onClick={onClick}>位置情報取得</button>
       <div>
         <p>latitude: {latitude}</p>
