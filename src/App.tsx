28be212b9a759d0633e71c83d97d966185fc6e13
oligeon-dev <oligeon.dev@gmail.com>
2024-01-24 10:33:58 +0900

    display lat and lon

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [latitude, setLatitude] = useState<number>();
+  const [longitude, setLongitude] = useState<number>();
   const onClick = () => {
     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
   };
 
-  const successCallback = () => {};
+  const successCallback = (position: GeolocationPosition) => {
+    setLatitude(position.coords.latitude);
+    setLongitude(position.coords.longitude);
+  };
   const errorCallback = () => {};
   return (
     <>
-      <div>version: 0.0.1</div>
+      <div>version: 0.0.2</div>
       <button onClick={onClick}>位置情報取得</button>
+      <div>
+        <p>latitude: {latitude}</p>
+        <p>longitude: {longitude}</p>
+      </div>
     </>
   );
 }
