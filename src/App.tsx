456badad7d6d9be2b6e26cf3ca88783317e79828
oligeon-dev <oligeon.dev@gmail.com>
2024-01-26 13:37:11 +0900

    delete maskable

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ function App() {
   };
   return (
     <>
-      <div>version: 0.0.7</div>
+      <div>version: 0.0.8</div>
       <p>permission state: {permission}</p>
       <button onClick={getPermissionState}>CHECK</button>
       <button onClick={onClick}>位置情報取得</button>
--- vite.config.ts
+++ vite.config.ts
@@ -18,13 +18,11 @@ export default defineConfig({
             src: "192.png",
             sizes: "192x192",
             type: "image/png",
-            purpose: "maskable",
           },
           {
             src: "512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "maskable",
           },
         ],
       },
