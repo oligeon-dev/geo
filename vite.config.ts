c294cb9b176f4c02a925e5d7a5e7908714e991f2
oligeon-dev <oligeon.dev@gmail.com>
2024-01-26 11:05:54 +0900

    maskable

--- vite.config.ts
+++ vite.config.ts
@@ -18,11 +18,13 @@ export default defineConfig({
             src: "192.png",
             sizes: "192x192",
             type: "image/png",
+            purpose: "maskable",
           },
           {
             src: "512.png",
             sizes: "512x512",
             type: "image/png",
+            purpose: "maskable",
           },
         ],
       },
